c614daf49a9fd11fbe48f89eae83326b85303f51
iftikhara158 <iftikhara158@gmail.com>
2025-10-12 10:47:56 +0500

    Update ChangeServerPage.jsx

--- src/login/ChangeServerPage.jsx
+++ src/login/ChangeServerPage.jsx
@@ -22,13 +22,7 @@ const currentServer = `${window.location.protocol}//${window.location.host}`;
 
 const officialServers = [
   currentServer,
-  'https://demo.traccar.org',
-  'https://demo2.traccar.org',
-  'https://demo3.traccar.org',
-  'https://demo4.traccar.org',
-  'https://server.traccar.org',
-  'http://localhost:8082',
-  'http://localhost:3000',
+  'https://server.atrack.com.pk',
 ].filter((value, index, self) => self.indexOf(value) === index);
 
 const useStyles = makeStyles()((theme) => ({
