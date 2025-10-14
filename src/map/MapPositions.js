6c818a7fd624e5f3138f7c61b2b87fd07d800b3e
iftikhara158 <iftikhara158@gmail.com>
2025-10-14 20:41:12 +0500

    Update MapPositions.js

--- src/map/MapPositions.js
+++ src/map/MapPositions.js
@@ -3,7 +3,6 @@ import { useSelector } from 'react-redux';
 import { useMediaQuery } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import { map } from './core/MapView';
-import { formatTime, getStatusColor } from '../common/util/formatter';
 import { mapIconKey } from './core/preloadImages';
 import { useAttributePreference } from '../common/util/preferences';
 import { useCatchCallback } from '../reactHelper';
@@ -13,7 +12,6 @@ const MapPositions = ({
   positions,
   onMapClick,
   onMarkerClick,
-  showStatus,
   selectedPosition,
   titleField,
 }) => {
